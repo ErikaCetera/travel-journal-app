a5eebd683f36e29fc2a3fef3315f142c8e88e5b4
Erika Cetera <ceteraerika@gmail.com>
2025-08-26 18:28:32 +0200

    feat: layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,24 @@
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar';
 
 const Layout = () => {
-  return(
+  return (
     <>
-    <main> <Outlet /></main>
-    </>
-  )
-}
+      <Navbar />
+      <header className="logo-header mt-3">
+        <div className="logo-wrapper">
+          <img src="/logo2.png" alt="Logo" className="logo-img" />
+          <h1 className="logo-title">IL MIO DIARIO DI VIAGGIO</h1>
+        </div>
+      </header>
 
-export default Layout
+      <div className="page-background">
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
